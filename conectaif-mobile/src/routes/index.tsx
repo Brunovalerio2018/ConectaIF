10083c23c72c82274c4441432915d97c2f4a7593
Bruno Machado <126995337+Brunovalerio2018@users.noreply.github.com>
2024-10-08 17:26:34 -0300

    Update index.tsx

--- conectaif-mobile/src/routes/index.tsx
+++ conectaif-mobile/src/routes/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import LoginHome from '../screens/Principal/Login';
+import RecuperarConta from '../screens/Principal/RecuperarConta';
 
 const Stack = createNativeStackNavigator();
 
@@ -9,11 +10,8 @@ const Routes = () => {
   return (
     <NavigationContainer>
     <Stack.Navigator initialRouteName="Login">
-      <Stack.Screen 
-        name="Login" 
-        component={LoginHome} 
-        options={{ headerShown: false }} 
-      />
+      <Stack.Screen name="Login" component={LoginHome} options={{ headerShown: false }} />
+      <Stack.Screen name='ReConta' component={RecuperarConta} />
     </Stack.Navigator>
   </NavigationContainer>
   );
