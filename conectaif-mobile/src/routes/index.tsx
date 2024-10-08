aeaac9329d520e3e94ea3fb98953075e4e4002e3
Bruno Machado <126995337+Brunovalerio2018@users.noreply.github.com>
2024-10-08 03:12:07 -0300

    Update index.tsx

--- conectaif-mobile/src/routes/index.tsx
+++ conectaif-mobile/src/routes/index.tsx
@@ -1,11 +1,22 @@
-    import {NavigationContainer}from '@react-navigation/native';
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import LoginHome from '../screens/Principal/Login';
 
-    import TabRoutes from './tab.routes';
+const Stack = createNativeStackNavigator();
 
-    export default function Routes(){
-        return(
-            <NavigationContainer>
-                <TabRoutes/>
-            </NavigationContainer>
-        )
-    }
+const Routes = () => {
+  return (
+    <NavigationContainer>
+    <Stack.Navigator initialRouteName="Login">
+      <Stack.Screen 
+        name="Login" 
+        component={LoginHome} 
+        options={{ headerShown: false }} 
+      />
+    </Stack.Navigator>
+  </NavigationContainer>
+  );
+};
+
+export default Routes;
