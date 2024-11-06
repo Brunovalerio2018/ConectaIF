4cbe4646720f187a7612986bf71d55b901972bce
Bruno Machado <126995337+Brunovalerio2018@users.noreply.github.com>
2024-11-06 17:13:28 -0300

    Update tab.routes.tsx

--- conectaif-mobile/src/routes/tab.routes.tsx
+++ conectaif-mobile/src/routes/tab.routes.tsx
@@ -9,59 +9,60 @@ import Reunioes from "../screens/Reunioes";
 import Documentos from "../screens/Documento";
 import Notificacoes from "../screens/Notificacoes";
 
-
 const Tab = createBottomTabNavigator();
 
+export default function TabRoutes() {
+  return (
+    <Tab.Navigator screenOptions={{ title: '' }}>
+      
+      <Tab.Screen  
+        name="Ocorrencias"
+        component={Ocorrencias}
+        options={{
+          tabBarIcon: () => <Feather name="alert-triangle" size={24} color="black" />
+        }}
+      />
 
-export default function TabRoutes(){
-    return(
-        <Tab.Navigator screenOptions={{ title: ''}}>
-            
-            <Tab.Screen  
-            name="Ocorrencias"
-            component={Ocorrencias}
-            options={{tabBarIcon: () => <Feather name="home"/>
+      <Tab.Screen 
+        name="Grupos" 
+        component={Grupo}
+        options={{
+          tabBarIcon: () => <Feather name="users" size={24} color="black" />
+        }}
+      />
+      
+      <Tab.Screen 
+        name="Documentos"
+        component={Documentos}
+        options={{
+          tabBarIcon: () => <Feather name="file-text" size={24} color="black" />
+        }}
+      />
 
+      <Tab.Screen 
+        name="Reunioes"
+        component={Reunioes}
+        options={{
+          tabBarIcon: () => <Feather name="calendar" size={24} color="black" />
+        }}
+      />
 
-            }}/>
+      <Tab.Screen 
+        name="Notificacoes"
+        component={Notificacoes}
+        options={{
+          tabBarIcon: () => <Feather name="bell" size={24} color="black" />
+        }}
+      />
 
-            <Tab.Screen 
-            name="Grupos" 
-            component={Grupo}
-            options={{tabBarIcon: () => <Feather name="home"/>
-
-            }}/>
-            
-            <Tab.Screen 
-            name="Documentos"
-            component={Documentos}
-            options={{tabBarIcon: () => <Feather name="home"/>}}
-            />
-
-            <Tab.Screen 
-            name="Reunioes"
-            component={Reunioes}
-            options={{tabBarIcon: () => <Feather name="home"/>}}
-            />
-
-
-            <Tab.Screen 
-            name="Notificacoes"
-            component={Notificacoes}
-            options={{tabBarIcon: () => <Feather name="home"/>}}
-            />
-
-            <Tab.Screen 
-            name="Perfil"
-            component={Perfil}
-            options={{tabBarIcon: () => <Feather name="home"/>}}
-            />
-
-            
-
-
-
-
-        </Tab.Navigator> 
-    )
+      <Tab.Screen 
+        name="Perfil"
+        component={Perfil}
+        options={{
+          tabBarIcon: () => <Feather name="user" size={24} color="black" />
+        }}
+      />
+      
+    </Tab.Navigator> 
+  );
 }
